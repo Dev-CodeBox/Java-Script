57ea5da92d52fb612a29497a53080a9d4bcb1ab6
Dev Raj Singh <152593087+Dev-CodeBox@users.noreply.github.com>
2024-11-01 12:19:59 +0000

    Using variables in JavaScript

---
+++ variables.js
@@ -0,0 +1,11 @@
+// Can't Be Changed Or Updated
+const myName = "Dev";
+
+// Used Like A Temp  Or Local Variable
+let age =  20;
+
+// Global Scoped Variable
+var email = "dev.maildrop@gmail.com";
+
+// Printing In Table Format In Console
+console.table([myName, age, email]);
